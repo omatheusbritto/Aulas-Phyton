d7c04105015a397c9617ada0594ee6caa1719a1d
Matheus Gonçalves De Britto <94484304+omatheusbritto@users.noreply.github.com>
2025-03-29 16:35:24 -0300

    Update main.py
    Alterações para incluir datas nas operações

--- main.py
+++ main.py
@@ -7,27 +7,34 @@ limitato á 3 saque por dias e com o valor maxímo de R$500 por saque
 o depósito não pode ser valor negativo,
 o extrato deve-se mostrar todas as transações realizada 
 '''
+from datetime import datetime
+
+agora = datetime.now()
+mascara_ptbr=('%d/%m/%Y %a %H:%M:%S')
+data_registrada = agora.strftime(mascara_ptbr)
+
 def linha():
     print(f'-'*31)
 def saque():
     global saldo    
     global limite_de_saque
-    global registro
-    saque = float(input('Valor a ser sacado R$'))
+    saque = float(input('Valor a ser sacado R$').replace(',','.'))
     if saque > 500:
-        print('ERRO - valor não permitido')
+        print('ERRO - maior que o permitido')
     elif saque < saldo and limite_de_saque < 3:
         saldo -= saque
         limite_de_saque += 1
-        if limite_de_saque == 3:
-            print(f'Limite de saque diário atingido')
-        registro = {'Saque R$': saque, 'Saldo R$': saldo}
+        registro = {'Data do saque :':data_registrada, 'Saque R$': f'{saque:.2f}', 
+                    'Saldo R$': f'{saldo:.2f}'
+                    }
         extrato.append(registro)
+    elif limite_de_saque == 3:
+        print(f'Limite de saque diário atingido')
     else:
-        print('ERRO!!! Valor de saque menor que saldo')
+        print('ERRO!!! Operação inválida')
 def mostrar_saldo():
     global saldo
-    print(f'Saldo atual em conta é de R${saldo:.2f}')
+    print(f' Data: {data_registrada} \n Saldo atual em conta é de R${saldo:.2f}')
 def deposito():
     global saldo
     global registro
@@ -35,15 +42,16 @@ def deposito():
     if deposito < 0:
         print('Valor inválido!!!')
         while deposito < 0:
-            deposito = float(input('Valor a ser depósitado').replace(',','.'))
-    registro = {'Saldo R$': saldo, 'Depósito R$': deposito}
+            deposito = float(input(f'Valor a ser depósitado').replace(',','.'))
+    registro = {'Data do depósito: ':data_registrada,'Saldo R$': f'{saldo:.2f}', 
+                'Depósito R$': f'{deposito:.2f}'}
     extrato.append(registro)
     saldo += deposito 
 def mostrar_extrato():
     global extrato
     for aux in extrato:
         for keys, value in aux.items():
-            print(f'{keys} {value:.2f}')
+            print(f'{keys} {value}')
         print('')
             
 
